e1b1b7342e2c226bf595816a3febe4f72b981a6f
perth pp <kekskcfm@gmail.com>
2020-08-23 12:43:38 +0700

    Add project files.

---
+++ week2/Source.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a, b, sum, ans;
+	scanf_s("%f %f", &a, &b);
+	sum = (a * a) + (b * b);
+	ans = sqrt(sum);
+	printf("%f", ans);
+	return 0;
+}
