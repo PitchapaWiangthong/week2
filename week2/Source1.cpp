a36c3fca193f0dd3ff89ac17c07f385b8801d967
perth pp <kekskcfm@gmail.com>
2020-08-23 13:20:31 +0700

    end

---
+++ week2/Source1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main1()
+{
+	int num, x = 2;
+	scanf_s("%d", &num);
+	while (num != 1)
+	{
+		while (num % x == 0)
+		{
+			printf("%d ", x);
+			num = num / x;
+		}
+		x++;
+	}
+	return 0;
+}
